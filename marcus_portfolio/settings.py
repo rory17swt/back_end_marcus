53cd62a9b34f6a04a32a5560b938ab96c98aba2f
rory_swt <rory@swietlicki.eu>
2025-12-17 16:48:18 +0000

    add rest_framework to installed apps

--- marcus_portfolio/settings.py
+++ marcus_portfolio/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'django_crontab',
     'corsheaders',
     'cloudinary',
