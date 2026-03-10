1dc38a5fc28270388b3d146829e7ff0e1d8dccae
rory_swt <rory@swietlicki.eu>
2026-03-10 15:15:53 +0000

    changed CORS & CRF for new netlify URL

--- marcus_portfolio/settings.py
+++ marcus_portfolio/settings.py
@@ -40,15 +40,13 @@ ALLOWED_HOSTS = [
     'localhost',
     'marcus-portfolio-api-2e75b5f2b384.herokuapp.com',
     '.herokuapp.com',
-    'tenor.swietlicki.eu',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
     'https://marcus-portfolio-front-2066569c5c21.herokuapp.com',
-    'https://tenor.swietlicki.eu',
-    'https://marcusportfoliosite.netlify.app',
+    'https://marcustenor.netlify.app',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -57,8 +55,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
     'https://marcus-portfolio-front-2066569c5c21.herokuapp.com',
-    'https://tenor.swietlicki.eu',
-    'https://marcusportfoliosite.netlify.app',
+    'https://marcustenor.netlify.app',
 ]
 
 # Application definition
